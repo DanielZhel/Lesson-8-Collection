63cd10b8abf0558fbe82d2cf5bce0a1fdc093ce6
Danielzhel <fartunazh@gmail.com>
2022-11-06 14:15:16 +0300

    Добавьте файлы проекта.

---
+++ Lesson 8 Collection/Collection.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8_Collection
+{
+    internal class Collection<T>
+    {
+        private T[] array;
+        private int size = 0;
+        private int i = 0;
+
+        public T this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+        public Collection()
+        {
+            this.array = new T[size];
+            
+        }
+        protected int Size
+        {
+            get { return size; }
+        }
+
+        public void Add(T value)
+        {
+            Array.Resize(ref this.array, array.Length+1);
+            size = array.Length;
+            array[i] = value;
+            i++;
+            
+        }
+        
+        public T GetValue(int index)
+        {
+            return array[index];
+        }
+
+        public void Remove(int index)
+        {
+            array[index] = default;
+        }
+
+        public int GetLength()
+        {
+            return size;
+        }
+
+    }
+
+    internal class MyCollection : Collection<int>
+    {
+       public static MyCollection operator +(MyCollection firstColl, MyCollection secondColl)
+        {
+            MyCollection result = new MyCollection();
+
+            if (firstColl.GetLength() != secondColl.GetLength())
+            {
+                Console.WriteLine("Different length's");
+            }
+            
+            for (int i = 0 ; i < firstColl.GetLength(); i++)
+            {
+                result.Add(firstColl[i] + secondColl[i]);
+            }
+            return result;
+        }
+
+        public static bool operator ==(MyCollection firstColl, MyCollection secondColl)
+        {
+            if (firstColl.GetLength() != secondColl.GetLength())
+            {
+                Console.WriteLine("Different length's");
+            }
+
+            for (int i = 0; i < firstColl.GetLength(); i++)
+            {
+                if (firstColl[i] != secondColl[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool operator !=(MyCollection firstColl, MyCollection secondColl)
+        {
+            if (firstColl.GetLength() != secondColl.GetLength())
+            {
+                Console.WriteLine("Different length's");
+            }
+
+            for (int i = 0; i < firstColl.GetLength(); i++)
+            {
+                if (firstColl[i] != secondColl[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+    
+}
---
+++ Lesson 8 Collection/Program.cs
@@ -0,0 +1,28 @@
+﻿using Lesson_8_Collection;
+
+MyCollection collection1 = new MyCollection();
+MyCollection collection2 = new MyCollection();
+
+collection1.Add(1);
+collection1.Add(2);
+collection1.Add(5);
+
+collection2.Add(1);
+collection2.Add(2);
+collection2.Add(5);
+
+
+
+Console.WriteLine(collection2.GetValue(2));
+
+
+Console.WriteLine($"{collection1.GetLength()} + {collection2.GetLength()}");
+
+var ans = collection1 + collection2;
+
+for (int i = 0; i < ans.GetLength(); i++)
+{
+    Console.WriteLine(ans[i]);
+}
+
+Console.WriteLine(collection1 == collection2);
